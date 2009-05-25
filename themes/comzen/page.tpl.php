c74c39e6ce226ac98d61197fb055230ccbfbc88b
Alex Scott <alex@arcx.co.uk>
2009-05-25 17:23:48 +0200

    Fix for VML namespace to work

--- themes/comzen/page.tpl.php
+++ themes/comzen/page.tpl.php
@@ -77,10 +77,16 @@
  * @see template_preprocess_page()
  */
 ?>
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language; ?>" lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>">
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+ <html  xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language; ?>" lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>">
 
 <head>
   <title><?php print $head_title; ?></title>
+  <!-- Quirksmode for vml namespace to work 
+  https://connect.microsoft.com/IE/feedback/ViewFeedback.aspx?FeedbackID=333905
+  -->
+  <meta http-equiv="X-UA-Compatible" content="IE=7" />
   <?php print $head; ?>
   <?php print $styles; ?>
   <!-- htc files need the full path written out for ie -->
